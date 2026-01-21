2f9a8b398da39b6d94d282599a2a2e22d4a19763
Ickarex <denchikkot.hom@gmail.com>
2026-01-22 05:06:13 +0700

    full reorganization

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Привет с Render! Python работает.'
+    return 'index.html'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
