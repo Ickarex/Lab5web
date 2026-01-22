c027f10c683f0c63d3134ce3fec77dfb9c5bbbfa
Ickarex <denchikkot.hom@gmail.com>
2026-01-22 19:58:24 +0700

    full reorganization

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,11 +1,45 @@
-from flask import Flask
+from flask import Flask, request, render_template, jsonify, send_from_directory
+from flask_cors import CORS
+import urllib.request
 import os
+import numpy as np
+from PIL import Image
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
-def hello():
-    return 'index.html'
+def index():
+    return render_template('index.html')
+
+@app.route("/api/changeimage", methods=['POST'])
+def change():
+    data = request.json
+    url = data.get('url')
+    if not url :
+        return jsonify({"error": "неверные данные"}), 400
+    print(url)
+    filename = "./flaskapp/resources/temp_image.jpg"
+    urllib.request.urlretrieve(url, filename)
+    loadedImage = Image.open(filename)
+    changedImage = changeImage(loadedImage)
+    changedImage.save(filename)
+    return jsonify({
+        "message": "succesful",
+        "url": "/getimage"
+        })
+
+#endpoint для получения локального обработанного изображения
+@app.route('/getimage')
+def getimage():
+    return send_from_directory('resources', 'temp_image.jpg')
+
+def changeImage(image):
+    print("Image is changing now")
+    image_arr = np.array(image)
+    image_arr = np.transpose(image_arr, (1,0,2))
+    res = Image.fromarray(image_arr)
+    return res
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
